6cbedf8209efcd9a8acb910023b53d5700c718de
ayushlordsage169-hash <ayushlordsage169@gmail.com>
2026-07-26 05:01:52 +0530

    Implement ping route to check server status
    Added a ping route that returns the current timestamp.

--- keep_alive.py
+++ keep_alive.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from threading import Thread
 import os
+import time
 
 app = Flask('')
 
 @app.route('/')
 def home():
     return "Bot is alive!"
+
+@app.route('/ping')
+def ping():
+    return str(time.time())
 
 def run():
     port = int(os.environ.get("PORT", 8080))
